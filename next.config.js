33237df474054f6a2eee91fc7a36e800de0e6ffd
Bilal <mb03086350364@gmail.com>
2024-01-17 01:12:14 +0500

    E-Commerce-Project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        API_URL:"http://localhost:3000",
+        DB_URI:"mongodb+srv://mianbilal:mianbilal123@cluster0.g8p1c6j.mongodb.net/buy-it-now"
+
+    },
+    images:{
+        domains:["res.cloudinary.com"],
+    }
+}
 
 module.exports = nextConfig
---
+++ pages/api/products/index.js
@@ -0,0 +1,13 @@
+import { createRouter } from "next-connect";
+import dbConnect from "@/backend/config/dbConnect";
+import { getProducts, newProduct } from "@/backend/controllers/productControllers";
+
+const router = createRouter();
+
+dbConnect();
+
+router.post(newProduct);
+router.get(getProducts);
+
+
+export default router.handler();
